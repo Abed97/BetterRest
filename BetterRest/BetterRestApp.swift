f736c07110482aec95813ab70f3ae11af8a5e64f
Abed Atassi <abed.s.atassi@gmail.com>
2021-09-13 19:14:13 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Abed Atassi on 2021-09-13.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
